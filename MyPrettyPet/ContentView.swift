0bfbf29ef2a2a8cf35ed134d3d8844ac33e86fb7
Buhra Taras <eroccked@gmail.com>
2026-01-07 13:43:57 +0200

    Initial Commit

---
+++ MyPrettyPet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyPrettyPet
+//
+//  Created by Taras Buhra on 07.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyPrettyPet/MyPrettyPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPrettyPetApp.swift
+//  MyPrettyPet
+//
+//  Created by Taras Buhra on 07.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyPrettyPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
