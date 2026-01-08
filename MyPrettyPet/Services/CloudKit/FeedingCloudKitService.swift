0359fc055a85aa9ec69f8b79a34640414c445f51
Buhra Taras <eroccked@gmail.com>
2026-01-08 18:25:09 +0200

    iCloudConnect

--- MyPrettyPet/Services/CloudKit/FeedingCloudKitService.swift
+++ MyPrettyPet/Services/CloudKit/FeedingCloudKitService.swift
@@ -5,32 +5,117 @@
 //  Created by Taras Buhra on 07.01.2026.
 //
 
-
 import Foundation
 import CloudKit
 
 class FeedingCloudKitService {
     
-    private var container: CKContainer?
-    private var privateDatabase: CKDatabase?
+    private let container = CKContainer.default()
+    private let privateDatabase: CKDatabase
     
     init() {
-        // Поки CloudKit не налаштований
+        self.privateDatabase = container.privateCloudDatabase
     }
     
+    // MARK: - Create Feeding Record
     func saveFeedingRecord(_ record: FeedingRecord, completion: @escaping (Result<FeedingRecord, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        let ckRecord = record.toCKRecord()
+        
+        privateDatabase.save(ckRecord) { savedRecord, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let savedRecord = savedRecord,
+                  let updatedRecord = FeedingRecord.fromCKRecord(savedRecord) else {
+                DispatchQueue.main.async {
+                    completion(.failure(CloudKitManager.CloudKitError.conversionFailed))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(updatedRecord))
+            }
+        }
     }
     
+    // MARK: - Fetch Feeding Records for Pet
     func fetchFeedingRecords(for petID: UUID, completion: @escaping (Result<[FeedingRecord], Error>) -> Void) {
-        completion(.success([]))
+        let predicate = NSPredicate(format: "petID == %@", petID.uuidString)
+        let query = CKQuery(recordType: "FeedingRecord", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "dateTime", ascending: false)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let feedingRecords = records.compactMap { FeedingRecord.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(feedingRecords))
+            }
+        }
     }
     
+    // MARK: - Fetch Recent Feeding Records (Last 24 hours)
     func fetchRecentFeedingRecords(for petID: UUID, completion: @escaping (Result<[FeedingRecord], Error>) -> Void) {
-        completion(.success([]))
+        let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: Date()) ?? Date()
+        
+        let predicate = NSPredicate(format: "petID == %@ AND dateTime >= %@", petID.uuidString, yesterday as NSDate)
+        let query = CKQuery(recordType: "FeedingRecord", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "dateTime", ascending: false)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let feedingRecords = records.compactMap { FeedingRecord.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(feedingRecords))
+            }
+        }
     }
     
+    // MARK: - Delete Feeding Record
     func deleteFeedingRecord(recordID: CKRecord.ID, completion: @escaping (Result<Void, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        privateDatabase.delete(withRecordID: recordID) { deletedRecordID, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        }
     }
 }
--- MyPrettyPet/Services/CloudKit/MedicalCloudKitService.swift
+++ MyPrettyPet/Services/CloudKit/MedicalCloudKitService.swift
@@ -11,70 +11,397 @@ import CloudKit
 
 class MedicalCloudKitService {
     
-    private var container: CKContainer?
-    private var privateDatabase: CKDatabase?
+    private let container = CKContainer.default()
+    private let privateDatabase: CKDatabase
     
     init() {
-        // Поки CloudKit не налаштований
+        self.privateDatabase = container.privateCloudDatabase
     }
     
     // MARK: - Vaccination
+    
+    /// Зберегти щеплення
     func saveVaccination(_ vaccination: Vaccination, completion: @escaping (Result<Vaccination, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        let record = vaccination.toCKRecord()
+        
+        privateDatabase.save(record) { savedRecord, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let savedRecord = savedRecord,
+                  let updatedVaccination = Vaccination.fromCKRecord(savedRecord) else {
+                DispatchQueue.main.async {
+                    completion(.failure(CloudKitManager.CloudKitError.conversionFailed))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(updatedVaccination))
+            }
+        }
     }
     
+    /// Отримати всі щеплення для тварини
     func fetchVaccinations(for petID: UUID, completion: @escaping (Result<[Vaccination], Error>) -> Void) {
-        completion(.success([]))
+        let predicate = NSPredicate(format: "petID == %@", petID.uuidString)
+        let query = CKQuery(recordType: "Vaccination", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "dateAdministered", ascending: false)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let vaccinations = records.compactMap { Vaccination.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(vaccinations))
+            }
+        }
     }
     
+    /// Отримати щеплення які потребують нагадування
     func fetchUpcomingVaccinations(for petID: UUID, completion: @escaping (Result<[Vaccination], Error>) -> Void) {
-        completion(.success([]))
+        let futureDate = Calendar.current.date(byAdding: .day, value: 30, to: Date()) ?? Date()
+        
+        let predicate = NSPredicate(format: "petID == %@ AND nextDueDate != nil AND nextDueDate <= %@ AND nextDueDate >= %@",
+                                   petID.uuidString,
+                                   futureDate as NSDate,
+                                   Date() as NSDate)
+        let query = CKQuery(recordType: "Vaccination", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "nextDueDate", ascending: true)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let vaccinations = records.compactMap { Vaccination.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(vaccinations))
+            }
+        }
     }
     
+    /// Видалити щеплення
     func deleteVaccination(recordID: CKRecord.ID, completion: @escaping (Result<Void, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        privateDatabase.delete(withRecordID: recordID) { deletedRecordID, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        }
     }
     
     // MARK: - Deworming
+    
+    /// Зберегти глистування
     func saveDeworming(_ deworming: Deworming, completion: @escaping (Result<Deworming, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        let record = deworming.toCKRecord()
+        
+        privateDatabase.save(record) { savedRecord, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let savedRecord = savedRecord,
+                  let updatedDeworming = Deworming.fromCKRecord(savedRecord) else {
+                DispatchQueue.main.async {
+                    completion(.failure(CloudKitManager.CloudKitError.conversionFailed))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(updatedDeworming))
+            }
+        }
     }
     
+    /// Отримати всі записи глистування для тварини
     func fetchDewormings(for petID: UUID, completion: @escaping (Result<[Deworming], Error>) -> Void) {
-        completion(.success([]))
+        let predicate = NSPredicate(format: "petID == %@", petID.uuidString)
+        let query = CKQuery(recordType: "Deworming", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "dateAdministered", ascending: false)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let dewormings = records.compactMap { Deworming.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(dewormings))
+            }
+        }
     }
     
+    /// Отримати глистування які потребують нагадування
     func fetchUpcomingDewormings(for petID: UUID, completion: @escaping (Result<[Deworming], Error>) -> Void) {
-        completion(.success([]))
+        let futureDate = Calendar.current.date(byAdding: .day, value: 14, to: Date()) ?? Date()
+        
+        let predicate = NSPredicate(format: "petID == %@ AND nextDueDate != nil AND nextDueDate <= %@ AND nextDueDate >= %@",
+                                   petID.uuidString,
+                                   futureDate as NSDate,
+                                   Date() as NSDate)
+        let query = CKQuery(recordType: "Deworming", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "nextDueDate", ascending: true)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let dewormings = records.compactMap { Deworming.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(dewormings))
+            }
+        }
     }
     
+    /// Видалити глистування
     func deleteDeworming(recordID: CKRecord.ID, completion: @escaping (Result<Void, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        privateDatabase.delete(withRecordID: recordID) { deletedRecordID, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        }
     }
     
     // MARK: - Flea Treatment
+    
+    /// Зберегти обробку від бліх
     func saveFleaTreatment(_ fleaTreatment: FleaTreatment, completion: @escaping (Result<FleaTreatment, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        let record = fleaTreatment.toCKRecord()
+        
+        privateDatabase.save(record) { savedRecord, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let savedRecord = savedRecord,
+                  let updatedFleaTreatment = FleaTreatment.fromCKRecord(savedRecord) else {
+                DispatchQueue.main.async {
+                    completion(.failure(CloudKitManager.CloudKitError.conversionFailed))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(updatedFleaTreatment))
+            }
+        }
     }
     
+    /// Отримати всі обробки від бліх для тварини
     func fetchFleaTreatments(for petID: UUID, completion: @escaping (Result<[FleaTreatment], Error>) -> Void) {
-        completion(.success([]))
+        let predicate = NSPredicate(format: "petID == %@", petID.uuidString)
+        let query = CKQuery(recordType: "FleaTreatment", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "dateAdministered", ascending: false)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let fleaTreatments = records.compactMap { FleaTreatment.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(fleaTreatments))
+            }
+        }
     }
     
+    /// Отримати обробки від бліх які потребують нагадування
     func fetchUpcomingFleaTreatments(for petID: UUID, completion: @escaping (Result<[FleaTreatment], Error>) -> Void) {
-        completion(.success([]))
+        let futureDate = Calendar.current.date(byAdding: .day, value: 7, to: Date()) ?? Date()
+        
+        let predicate = NSPredicate(format: "petID == %@ AND nextDueDate != nil AND nextDueDate <= %@ AND nextDueDate >= %@",
+                                   petID.uuidString,
+                                   futureDate as NSDate,
+                                   Date() as NSDate)
+        let query = CKQuery(recordType: "FleaTreatment", predicate: predicate)
+        query.sortDescriptors = [NSSortDescriptor(key: "nextDueDate", ascending: true)]
+        
+        privateDatabase.perform(query, inZoneWith: nil) { records, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            guard let records = records else {
+                DispatchQueue.main.async {
+                    completion(.success([]))
+                }
+                return
+            }
+            
+            let fleaTreatments = records.compactMap { FleaTreatment.fromCKRecord($0) }
+            
+            DispatchQueue.main.async {
+                completion(.success(fleaTreatments))
+            }
+        }
     }
     
+    /// Видалити обробку від бліх
     func deleteFleaTreatment(recordID: CKRecord.ID, completion: @escaping (Result<Void, Error>) -> Void) {
-        completion(.failure(CloudKitManager.CloudKitError.iCloudAccountNotAvailable))
+        privateDatabase.delete(withRecordID: recordID) { deletedRecordID, error in
+            if let error = error {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+                return
+            }
+            
+            DispatchQueue.main.async {
+                completion(.success(()))
+            }
+        }
     }
     
-    // MARK: - Combined
+    // MARK: - Combined Medical Records
+    
+    /// Отримати всі медичні записи які потребують нагадування
     func fetchAllUpcomingMedicalRecords(for petID: UUID, completion: @escaping (Result<MedicalReminders, Error>) -> Void) {
-        let reminders = MedicalReminders(vaccinations: [], dewormings: [], fleaTreatments: [])
-        completion(.success(reminders))
+        let group = DispatchGroup()
+        
+        var vaccinations: [Vaccination] = []
+        var dewormings: [Deworming] = []
+        var fleaTreatments: [FleaTreatment] = []
+        
+        var fetchError: Error?
+        
+        // Fetch vaccinations
+        group.enter()
+        fetchUpcomingVaccinations(for: petID) { result in
+            switch result {
+            case .success(let items):
+                vaccinations = items
+            case .failure(let error):
+                fetchError = error
+            }
+            group.leave()
+        }
+        
+        // Fetch dewormings
+        group.enter()
+        fetchUpcomingDewormings(for: petID) { result in
+            switch result {
+            case .success(let items):
+                dewormings = items
+            case .failure(let error):
+                fetchError = error
+            }
+            group.leave()
+        }
+        
+        // Fetch flea treatments
+        group.enter()
+        fetchUpcomingFleaTreatments(for: petID) { result in
+            switch result {
+            case .success(let items):
+                fleaTreatments = items
+            case .failure(let error):
+                fetchError = error
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            if let error = fetchError {
+                completion(.failure(error))
+            } else {
+                let reminders = MedicalReminders(
+                    vaccinations: vaccinations,
+                    dewormings: dewormings,
+                    fleaTreatments: fleaTreatments
+                )
+                completion(.success(reminders))
+            }
+        }
     }
     
+    // MARK: - Helper Struct
     struct MedicalReminders {
         let vaccinations: [Vaccination]
         let dewormings: [Deworming]
